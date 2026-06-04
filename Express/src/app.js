db25e8b9f872a34fe43a4e3112a57602b438bb28
Aditya <adityanugraha7251@gmail.com>
2026-06-04 15:53:44 +0700

    testing app

--- Express/src/app.js
+++ Express/src/app.js
@@ -1,19 +1,12 @@
 import express from 'express';
-import { ErrorHandler } from './middlewares/Error.js';
-import router from './routes/index.js';
 
 const app = express();
 
-// body parser
-app.use(express.json());
-
-// static uploads
-app.use('/uploads', express.static('src/uploads'));
-
-// routes
-app.use(router);
-
-// error handler HARUS paling bawah
-app.use(ErrorHandler);
+app.get('/', (req,res)=>{
+  res.status(200).json({
+    success:true,
+    message:'API running'
+  });
+});
 
 export default app;
