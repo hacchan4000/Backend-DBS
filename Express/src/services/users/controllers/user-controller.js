ef3adf6a7eafad668ab3d07995d3b2503bed4275
Aditya <adityanugraha7251@gmail.com>
2026-06-04 15:20:19 +0700

    gapake redis dlu

--- Express/src/services/users/controllers/user-controller.js
+++ Express/src/services/users/controllers/user-controller.js
@@ -1,5 +1,4 @@
 import { NotFoundError } from '../../../exceptions/index.js';
-import redisClient from '../../../utils/redis.js';
 import response from "../../../utils/response.js"
 import userRepo from "../repositories/user-repo.js"
 
@@ -27,21 +26,7 @@ export const getUserById = async(req,res,next) => {
 
     const { id } = req.params;
 
-    const cacheKey = `users:${id}`
-    const cache = await redisClient.get(cacheKey)
-    if (cache) {
-      res.set(
-        'X-Data-Source',
-        'cache'
-      );
-
-      return response(
-        res,
-        200,
-        'berhasil ambil users',
-        JSON.parse(cache)
-      );
-    }
+    
     const user = await userRepo.read(id);
 
     if(!user){
@@ -51,12 +36,7 @@ export const getUserById = async(req,res,next) => {
         )
       );
     }
-    await redisClient.setEx(cacheKey,3600,JSON.stringify(user))
 
-    res.set(
-      'X-Data-Source',
-      'database'
-    );
 
     return response(
       res,
