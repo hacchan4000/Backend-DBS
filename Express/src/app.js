cfa9515cb8d8af4cc0875c24eaa6bb9c470e97e2
Aditya <adityanugraha7251@gmail.com>
2026-06-04 15:48:14 +0700

    benerin app

--- Express/src/app.js
+++ Express/src/app.js
@@ -1,16 +1,19 @@
 import express from 'express';
-import routes from './routes/index.js';
 import { ErrorHandler } from './middlewares/Error.js';
+import router from './routes/index.js';
 
 const app = express();
 
 // body parser
 app.use(express.json());
 
-// static upload kalau nanti ada PDF
+// static uploads
 app.use('/uploads', express.static('src/uploads'));
 
-// error handler (harus paling bawah)
+// routes
+app.use(router);
+
+// error handler HARUS paling bawah
 app.use(ErrorHandler);
 
 export default app;
--- Express/src/routes/index.js
+++ Express/src/routes/index.js
@@ -2,13 +2,19 @@ import express from 'express';
 import userRoutes from '../services/users/routes/index.js';
 import authRoutes from '../services/authentications/routes/index.js';
 import documentRoutes from '../services/documents/routes/index.js';
+import response from '../utils/response.js';
 
 const router = express.Router();
 
+router.get('/',(req,res)=>{
+  return response(res, 200, 'API berhasil jalan', {success:true})
+})
 router.use('/users', userRoutes);
 router.use('/authentications', authRoutes);
 router.use('/documents', documentRoutes);
 //router.use('/predict')
+
+
 
 
 export default router
