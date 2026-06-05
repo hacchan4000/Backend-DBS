6d8483f88c11da89e0f8f879306e57a5d24163c1
Aditya <adityanugraha7251@gmail.com>
2026-06-05 17:11:30 +0700

    add cors to app

--- Express/src/app.js
+++ Express/src/app.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import { ErrorHandler } from './middlewares/Error.js';
 import router from './routes/index.js';
+import cors from 'cors';
+
 
 const app = express();
 
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://smart-finance-tawny.vercel.app', // ganti dengan domain Vercel kamu
+  ],
+  credentials: true,
+}));
 // body parser
 app.use(express.json());
 
