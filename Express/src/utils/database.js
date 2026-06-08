ac16affb87ba751d62e6ec22fb86aea6f6a07367
Aditya <adityanugraha7251@gmail.com>
2026-06-08 16:33:28 +0700

    fix database

--- Express/src/utils/database.js
+++ Express/src/utils/database.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = new Pool({
-    connectionString: "postgresql://postgres:AdityA42069chAndrA@db.ophkitxgeobubdciwgpa.supabase.co:5432/postgres",
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
