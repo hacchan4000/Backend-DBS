8b6dc9d95ab460db718d1f8d934339499b821b12
Aditya <adityanugraha7251@gmail.com>
2026-06-12 18:10:37 +0700

    fix abit

--- Express/src/services/purchases/validator/schema.js
+++ Express/src/services/purchases/validator/schema.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const purchaseSchema = Joi.object({
   title: Joi.string().required(),
-  category: Joi.string().required(),
+  category_id: Joi.string().required(),
   date: Joi.date().required(),
   price: Joi.number().required(),
 })
